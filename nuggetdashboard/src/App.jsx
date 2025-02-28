b2c568035fa04a03d9996fad6eaa81457b8cf0fc
Burtarn <robin.andersson0125@gmail.com>
2025-02-28 11:42:35 +0100

    Footer component done.

--- nuggetdashboard/src/App.jsx
+++ nuggetdashboard/src/App.jsx
@@ -1,5 +1,8 @@
 import { RouterProvider } from "react-router-dom";
 import route from "./routing/Routing";
+import './index.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+
 
 function App() {
   return <RouterProvider router={route} />;
--- nuggetdashboard/src/routing/RootLayout.jsx
+++ nuggetdashboard/src/routing/RootLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from '../components/Footer'
 function RootLayout() {
     return ( <>
 <header></header>
 <main>
     <Outlet/>
 </main>
-<footer></footer>
+< Footer />
     </> );
 }
 
