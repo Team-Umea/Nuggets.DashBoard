9b97b9631218bac077fe25e304a5b712ca087122
FrankNorq <168375928+FrankNorq@users.noreply.github.com>
2025-03-07 10:08:58 +0100

    nonimportant

--- nuggetdashboard/src/components/Header/Navbar.jsx
+++ nuggetdashboard/src/components/Header/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
                 <div className="relative flex items-center justify-between h-16">
                     <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                         <button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-                            <span className="sr-only">Öppna huvudmeny</span>
+                            <span className="sr-only">Open Menu</span>
                             <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" aria-hidden="true">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
                             </svg>
