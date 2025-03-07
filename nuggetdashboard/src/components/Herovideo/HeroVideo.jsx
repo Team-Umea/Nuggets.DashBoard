35fa4ce817fb144116dc21e040aa0cb2cc839ce6
Burtarn <robin.andersson0125@gmail.com>
2025-03-07 10:05:31 +0100

    Update HeroVideo.jsx

--- nuggetdashboard/src/components/Herovideo/HeroVideo.jsx
+++ nuggetdashboard/src/components/Herovideo/HeroVideo.jsx
@@ -1,18 +1,20 @@
-    import React from 'react';
+// src/HeroVideo.jsx
+import React from 'react';
 
-    const HeroVideo = () => {
-    return (
-        <div className="relative overflow-hidden">
-        <video className="w-full h-auto" autoPlay loop muted>
-            <source src="./public/MyVideo.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-        </video>
-        <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white text-center">
-            <h1 className="text-4xl font-bold">BITCOIN BROS</h1>
-            <p className="mt-4 text-lg">TO THE MOOOOOOOOOOOON!</p>
-        </div>
-        </div>
-    );
-    };
+const HeroVideo = () => {
+  return (
+    <div className="relative overflow-hidden">
+      <video className="w-full h-auto" autoPlay loop muted>
+        <source src="/hero-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white text-center">
+        <h1 className="text-4xl font-bold">BITCOIN BROS!</h1>
+        <p className="mt-4 text-lg">Lets go to the moooooon!</p>
 
-    export default HeroVideo;
+      </div>
+    </div>
+  );
+};
+
+export default HeroVideo;
