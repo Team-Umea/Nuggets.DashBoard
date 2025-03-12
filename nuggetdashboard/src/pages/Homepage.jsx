e4078f818421b355f08a9cb555e655c236eb107f
Burtarn <robin.andersson0125@gmail.com>
2025-03-12 15:28:37 +0100

    Update charts.

--- nuggetdashboard/src/pages/Homepage.jsx
+++ nuggetdashboard/src/pages/Homepage.jsx
@@ -1,14 +1,25 @@
-
-import HeroVideo from '../components/Herovideo/HeroVideo'
-import FetchBitcoinData from '../components/FetchBitcoinData/FetchBitcoinData' 
-
-
+import HeroVideo from '../components/Herovideo/HeroVideo';
+import BasicPie from '../components/charts/PieChart';
+import BasicLineChart from '../components/charts/LineChart';
+import SimpleLineChart from '../components/charts/LineChartDeluxe';
+import CircularText from '../components/RotatingText/CircularText'
 
 function HomePage() {
   return (
     <div>
-  
-      < HeroVideo />
+      
+      <HeroVideo />
+      <h2 className="text-2xl font-bold text-center text-black">
+        Important things in life
+      </h2>
+      <div className="flex justify-center gap-8">
+        <BasicPie />
+        <SimpleLineChart />
+
+      </div>
+      <section className='mt-5'>
+      <CircularText text="Bitcoin"/>
+      </section>
 
     </div>
   );
