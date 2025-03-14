9e1af2ec049c07c27db5c1eb8eaf06adb3890fd6
Tobias Engvall <tobias.engvall@chasacademy.se>
2025-03-14 09:30:19 +0100

    changed name of products and calc

--- nuggetdashboard/src/components/Header/Navbar.jsx
+++ nuggetdashboard/src/components/Header/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
 
   const links = [
     { name: "Home", path: "/" },
-    { name: "Products", path: "/products" },
-    { name: "Calculate Savings", path: "/calc" },
+    { name: "Bitcoin Calculator", path: "/products" },
+    { name: "Index Calculator", path: "/calc" },
     { name: "About us", path: "/about" },
   ];
 
